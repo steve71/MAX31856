9faea58a7ed3131980c10d2b5e1853532ed6e38b
Stephen Smith <steve028@yahoo.com>
2015-12-20 16:05:51 -0500

    Update max31856.py

--- max31856.py
+++ max31856.py
@@ -1,10 +1,32 @@
 #!/usr/bin/python
+#The MIT License (MIT)
+#
+#Copyright (c) 2015 Stephen Smith
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+
 import time, math
 import RPi.GPIO as GPIO
 
 class max31856(object):
-	"""Read Temperature from the MAX31865 chip with GPIO
-	   Any pins can be used.
+	"""Read Temperature on the Raspberry PI from the MAX31856 chip using GPIO
+	   Any pins can be used for CS (chip select), MISO, MOSI and CLK
 	"""
 
 	def __init__(self, csPin = 8, misoPin = 9, mosiPin = 10, clkPin = 11):
